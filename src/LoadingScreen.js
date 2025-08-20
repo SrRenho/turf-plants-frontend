323910133ff466d91d9ddc9e9bcc361d08a7d83f
SrRenho <juanjogvizioli@gmail.com>
2025-08-20 19:57:44 -0300

    animated elipsis in server load

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -1,9 +1,15 @@
+import Dots from "./Dots";
+
 export default function LoadingScreen() {
     return (
         <div style={{textAlign: 'center', marginTop: '50px'}}>
-            Server loading...<br/>
-            Free tier hosting may take a minute!<br/>
-            Be patient
+            Server loading<Dots /><br/>
+            No one joined for a while so the server was sleeping.<br/>
+            Free tier hosting servers may take a minute to wake up!<br/>
+            Thanks for your patience!
+            <br/>
+            <br/>
+            Turf Plants
         </div>
     );
 }

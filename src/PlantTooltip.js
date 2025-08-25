b09805e175c2a3156c9f390e002c4009f13d1a61
SrRenho <juanjogvizioli@gmail.com>
2025-08-25 19:47:39 -0300

    tooltip level caps at 100

--- src/PlantTooltip.js
+++ src/PlantTooltip.js
@@ -30,10 +30,12 @@ export default function PlantTooltip({ visible, info, pos }) {
           <div style={{ display: "flex", justifyContent: "space-between", fontSize: "0.8em", color: "gray" }}>
             <span>({info.x.toFixed(1)}, {info.y.toFixed(1)})</span>
             <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
-              <span>Lvl: {info.level}</span>
+            <span>Lvl: {info.level}</span>
+            {info.level < 100 && (
               <span>
                 Next: {Math.floor(info.xp_into_level)} / {Math.floor(info.xp_until_next)} XP
               </span>
+            )}
             </div>
           </div>
           <br />

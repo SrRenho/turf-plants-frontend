53726be660406a8576b582703147f7b86fab4995
SrRenho <juanjogvizioli@gmail.com>
2025-08-25 19:04:47 -0300

    amend welcome banner

--- src/WelcomeBanner.js
+++ src/WelcomeBanner.js
@@ -4,13 +4,14 @@ export default function WelcomeBanner() {
     const { user } = useAuth();
     return (
         <h3 style={{ textAlign: "left" }}>
-            {user ? (
-            user.seeds > 0
-                ? `Hello ${user.first_name || user.username || user.name}. You have ${user.seeds} seeds left. Click anywhere to plant!`
-                : "You've planted all your seeds. Thanks for leaving your mark in Turf Plants!"
-            ) : (
-            "Login to plant!"
-            )}
+            {user
+            ? `Hello ${user.first_name || user.username || user.name}. ${
+                user.seeds > 0
+                    ? `You have ${user.seeds} seeds left. Click anywhere to plant!`
+                    : "You've planted all your seeds. Thanks for leaving your mark in Turf Plants!"
+                }`
+            : "Login to plant!"
+            }
         </h3>
     );
 }

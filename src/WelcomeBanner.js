2fa9b10d8cdfa2e9cd2c7e19df102e46377ee2ef
SrRenho <juanjogvizioli@gmail.com>
2025-08-25 18:13:44 -0300

    better welcome banner

--- src/WelcomeBanner.js
+++ src/WelcomeBanner.js
@@ -3,6 +3,14 @@ import { useAuth } from "./AuthProvider";
 export default function WelcomeBanner() {
     const { user } = useAuth();
     return (
-        <h3  style={{ textAlign: "left" }}>{user ? `Hello ${user.first_name || user.username || user.name}. You have ${user.seeds} seeds left. Click anywhere to plant!` : 'Login to plant!'}</h3>
+        <h3 style={{ textAlign: "left" }}>
+            {user ? (
+            user.seeds > 0
+                ? `Hello ${user.first_name || user.username || user.name}. You have ${user.seeds} seeds left. Click anywhere to plant!`
+                : "You've planted all your seeds. Thanks for leaving your mark in Turf Plants!"
+            ) : (
+            "Login to plant!"
+            )}
+        </h3>
     );
 }
